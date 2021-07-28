ed44ed221313e463f26d2f45a03adddbd595a67d
sean <00smarkey@gmail.com>
2021-07-28 14:30:20 -0400

    working CNN model

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from load_data import edf_to_csv
 from util import split_dataframe, sample
 import pandas as pd
-from keras.utils import to_categorical, np_utils
+# from keras.utils import to_categorical, np_utils
 import numpy as np
 from model import CNN1D
 
@@ -14,6 +14,11 @@ cleaned_str = "_cleaned" if cleaned else ""
 normalized = True
 normalized_str = "_normalized" if cleaned else ""
 
+epochs = 6
+batch_size = 4
+input_len = 1
+nr_params = 10
+
 if __name__ == '__main__':
 
     try:
@@ -25,7 +30,10 @@ if __name__ == '__main__':
 
     # print(df)
     del df['Unnamed: 0']
-    batches = split_dataframe(df, batch_size=2880)
+    df['age'] = pd.Series(df['age'].values.astype(np.float32))
+    df['gender'] = pd.Series(df['gender'].values.astype(np.float32))
+    del df['age']
+    batches = split_dataframe(df, batch_size=batch_size)
     data = [batch.values for batch in batches]
 
     train_data, test_data = sample(data, .9)
@@ -38,10 +46,40 @@ if __name__ == '__main__':
     train_in, train_out = map(list, zip(*[split(elt) for elt in train_data]))
     test_in, test_out = map(list, zip(*[split(elt) for elt in test_data]))
 
-    train_out = [pd.Series(elt, dtype='category').values for elt in train_out]
-    test_out = [pd.Series(elt, dtype='category').values for elt in test_out]
-    print(train_data)
-    # cnn = CNN1D((10, 1))
+    train_in = np.concatenate(train_in)
+    train_out = np.concatenate(train_out)
+    test_in = np.concatenate(test_in)
+    test_out = np.concatenate(test_out)
+
+    # train_out = [pd.get_dummies(elt).values for elt in train_out]
+    # test_out = [pd.get_dummies(elt).values for elt in test_out]
+
+    train_out = pd.get_dummies(train_out)
+    test_out = pd.get_dummies(test_out)
+
+    # train_in = [np.asarray(elt).astype(np.float32) for elt in train_in]
+    # test_in = [np.asarray(elt).astype(np.float32) for elt in test_in]
+
+    train_in = np.asarray(train_in).astype(np.float32)
+    test_in = np.asarray(test_in).astype(np.float32)
+
+    # train_in.reshape(train_in.shape[0], )
+    # train_out = np.asarray(train_out).astype(np.float32)
+
+
+
+    # print(train_data)
+    cnn = CNN1D((input_len, nr_params))
+    train_in = train_in.reshape(train_in.shape[0]//input_len, input_len, nr_params)
+    train_out = np.asarray(train_out).reshape(train_out.shape[0]//input_len, 4)
+
+    test_in = test_in.reshape(test_in.shape[0]//input_len, input_len, nr_params)
+    test_out = np.asarray(test_out).reshape(test_out.shape[0]//input_len, 4)
+    cnn.fit(train_in, train_out, batch_size=batch_size, epochs=epochs)
+
+    score = cnn.evaluate(test_in, test_out)
+    print('accuracy: ', score[1] * 100, '%')
+
     # print(data)
 
     # print(df)
--- model.py
+++ model.py
@@ -1,11 +1,14 @@
 from keras.layers import Dense, Conv1D, MaxPooling2D, Flatten, MaxPooling1D
 from keras.models import Sequential
+from keras.optimizers import Adam
 
 def CNN1D(input_shape: tuple) -> Sequential:
     model = Sequential()
     model.add(Conv1D(filters=64, kernel_size=1, activation='relu', input_shape=input_shape))
-    model.add(MaxPooling1D(pool_size=2))
+    model.add(MaxPooling1D(pool_size=1))
     model.add(Flatten())
-    model.add(Dense(5, activation='softmax'))
+    model.add(Dense(50))
+    model.add(Dense(4, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.0006), metrics=['accuracy'])
     model.summary()
     return model
