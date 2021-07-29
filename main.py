c25eaa4ef8116b0187c50febd11082a3adc88022
sean <00smarkey@gmail.com>
2021-07-29 15:58:43 -0400

    added simple feedforward model

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import pandas as pd
 # from keras.utils import to_categorical, np_utils
 import tensorflow as tf
 import numpy as np
-from model import CNN1D
+from model import CNN1D, SimpleFF
 
-trimmed = False
+trimmed = True
 trimmed_str = "_trimmed" if trimmed else ""
 
 cleaned = True
 cleaned_str = "_cleaned" if cleaned else ""
 
-normalized = False
+normalized = True
 normalized_str = "_normalized" if normalized else ""
 
 predicting = True
 
-epochs = 8
-batch_size = 20
+epochs = 14
+batches_per_epoch = 4616
 input_len = 1
-nr_params = 10
+nr_params = 12
 
 if __name__ == '__main__':
 
@@ -31,11 +31,18 @@ if __name__ == '__main__':
         edf_to_csv(trimmed=trimmed, cleaned=cleaned, normalized=normalized)
         df = pd.read_csv(f"data/edf_data{trimmed_str}{cleaned_str}{normalized_str}.csv")
 
+    batch_size = (len(df) // batches_per_epoch)
+
+    to_del = ['Unnamed: 0', 'age', 'gender', 'rr_trend', 'rs_mean', 'rs_std', 'rr_range']
+
+    df = df.drop(to_del, axis=1)
+    nr_params -= len(to_del)
+
     # print(df)
-    del df['Unnamed: 0']
-    df['age'] = pd.Series(df['age'].values.astype(np.float32))
-    df['gender'] = pd.Series(df['gender'].values.astype(np.float32))
-    del df['age']
+    # del df['Unnamed: 0']
+    # df['age'] = pd.Series(df['age'].values.astype(np.float32))
+    # df['gender'] = pd.Series(df['gender'].values.astype(np.float32))
+    # del df['age']
     batches = split_dataframe(df, batch_size=batch_size)
     data = [batch.values for batch in batches]
 
@@ -72,15 +79,18 @@ if __name__ == '__main__':
 
 
     # print(train_data)
-    cnn = CNN1D((input_len, nr_params))
+    # model = SimpleFF((nr_params,))
+    model = CNN1D((input_len, nr_params))
     train_in = train_in.reshape(train_in.shape[0]//input_len, input_len, nr_params)
+    # train_in = train_in.reshape(train_in.shape[0] // input_len, nr_params)
     train_out = np.asarray(train_out).reshape(train_out.shape[0]//input_len, 4)
 
     test_in = test_in.reshape(test_in.shape[0]//input_len, input_len, nr_params)
+    # test_in = test_in.reshape(test_in.shape[0] // input_len, nr_params)
     test_out = np.asarray(test_out).reshape(test_out.shape[0]//input_len, 4)
-    cnn.fit(train_in, train_out, batch_size=batch_size, epochs=epochs)
+    model.fit(train_in, train_out, batch_size=batch_size, epochs=epochs)
 
-    score = cnn.evaluate(test_in, test_out)
+    score = model.evaluate(test_in, test_out)
     print('accuracy: ', score[1] * 100, '%')
 
     if predicting:
@@ -93,9 +103,10 @@ if __name__ == '__main__':
         predict_out = pd.get_dummies(predict_out)
 
         predict_in = predict_in.reshape(predict_in.shape[0] // input_len, input_len, nr_params)
+        # predict_in = predict_in.reshape(predict_in.shape[0] // input_len, nr_params)
         predict_out = np.asarray(predict_out).reshape(predict_out.shape[0] // input_len, 4)
 
-        output = cnn.predict(predict_in, batch_size=batch_size)
+        output = model.predict(predict_in, batch_size=batch_size)
 
         m = tf.keras.metrics.CategoricalAccuracy()
         m.update_state(y_true=predict_out, y_pred=output)
@@ -104,19 +115,12 @@ if __name__ == '__main__':
         predictions = np.array([elt.tolist().index(max(elt)) for elt in output])
         labels = np.array([['awake', 'light', 'deep', 'rem'][val] for val in predictions])
         df['out_label'] = labels
-        df.join(pd.DataFrame(output, columns=['awake%', 'light%', 'deep%', 'rem%']))
 
         incorr = np.where((df['label'] != df['out_label']), 1, 0)
         total_wrong = np.sum(incorr)
-        print(f"Categorical Accuracy after prediction (manually computed): {1 - (total_wrong/len(predictions))}")
+        print(f"Categorical Accuracy after prediction (manually computed): {1 - (total_wrong / len(predictions))}")
         df['incorr'] = incorr
 
+        df = df.join(pd.DataFrame(output, columns=['awake%', 'light%', 'deep%', 'rem%']))
         df.to_csv(f"data/edf_data{trimmed_str}{cleaned_str}{normalized_str}_output.csv")
-
-
-        # labels = pd.Series(labels)
-
-
-    # print(data)
-
-    # print(df)
+        print('done!')
--- model.py
+++ model.py
@@ -2,15 +2,22 @@ from keras.layers import Dense, Conv1D, MaxPooling2D, Flatten, MaxPooling1D, Dro
 from keras.models import Sequential
 from keras.optimizers import Adam
 
+def SimpleFF(input_shape: tuple) -> Sequential:
+    model = Sequential()
+    model.add(Input(shape=input_shape))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(4, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.005), metrics=['categorical_accuracy'])
+    model.summary()
+    return model
+
 def CNN1D(input_shape: tuple) -> Sequential:
     model = Sequential()
     model.add(Conv1D(filters=64, kernel_size=1, activation='relu', input_shape=input_shape))
     model.add(MaxPooling1D(pool_size=1))
-    # model.add(Input(shape=input_shape))
-    # model.add(Dense(300))
     model.add(Flatten())
     model.add(Dense(50, activation='relu'))
     model.add(Dense(4, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.0006), metrics=['categorical_accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.0012), metrics=['categorical_accuracy'])
     model.summary()
     return model
