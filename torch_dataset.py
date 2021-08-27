38d4077440d9b5511b6461d5168fdbea382e7d75
sean <00smarkey@gmail.com>
2021-08-27 12:36:22 -0400

    documentation for model and dataset

--- torch_dataset.py
+++ torch_dataset.py
@@ -1,6 +1,15 @@
-import torch
 import numpy as np
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
+
+"""
+Author: Sean Markey (00smarkey@gmail.com)
+
+Created: August 25th, 2021
+
+This file holds the definition for my custom pytorch dataset. This allows me to have a sliding window of points around
+each timestep for the purpose of training my LSTM. 
+"""
+
 
 class SlidingWindowDataset(Dataset):
 
@@ -11,6 +20,7 @@ class SlidingWindowDataset(Dataset):
         self.transform = transform
         self._mask, self._valid_indices = self._mark_windows()
 
+    # mark the timesteps which have invalid windows (len < window_size or containing points from more than one night)
     def _mark_windows(self):
 
         mask_arr = np.ones(len(self.input_arr))
@@ -18,28 +28,26 @@ class SlidingWindowDataset(Dataset):
         for index, inputs in enumerate(self.input_arr):
             window = self.input_arr[max((index - self.window_size), 0):index].tolist()
 
-            if len(window) < self.window_size:
+            if len(window) < self.window_size:  # window is not long enough
                 mask_arr[index] = 0
-            elif len(set([elt[-1] for elt in window])) > 1:
+            elif len(set([elt[-1] for elt in window])) > 1:  # window contains points from more than one night
                 mask_arr[index] = 0
 
         # remove the final day_num variable in the input array after use
         self.input_arr = self.input_arr[:, :-1]
-
-        # drop indices identified earlier in mask
-        # self.input_arr = self.input_arr[mask_arr == 1]
-        # self.label_arr = self.label_arr[mask_arr == 1]
 
         return mask_arr, np.nonzero(mask_arr)[0]
 
     def __len__(self):
         return len(self._valid_indices)
 
+    # implementing function to get tuple of data by index
     def __getitem__(self, index) -> tuple:
 
+        # use map to only take valid window labels and inputs
         index_map = self._valid_indices[index]
 
-        label = self.label_arr[index_map-1]
+        label = self.label_arr[index_map - 1]
         inputs = self.input_arr[max((index_map - self.window_size), 0):index_map]
 
         return inputs, label
--- torch_model.py
+++ torch_model.py
@@ -1,12 +1,20 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 from torchinfo import summary
 
+"""
+Author: Sean Markey (00smarkey@gmail.com)
 
+Created: August 16th, 2021
+
+This file holds the class definitions for all of my pytorch models.
+"""
+
+# this is a simple feed-forward model I used to get a baseline for how a model could work without time dependency
 class SimpleFF(nn.Module):
 
+    # just four fully connected layers
     def __init__(self, input_shape: tuple):
         super(SimpleFF, self).__init__()
         self.fc1 = nn.Linear(input_shape[0], 20)
@@ -14,6 +22,7 @@ class SimpleFF(nn.Module):
         self.fc3 = nn.Linear(30, 10)
         self.fc4 = nn.Linear(10, 4)
 
+    # apply ReLU after every fully connected layer, and then softmax on the outputs
     def forward(self, x):
         x = self.fc1(x)
         x = F.relu(x)
@@ -30,6 +39,7 @@ class SimpleFF(nn.Module):
         return output
 
 
+# this is the start of an LSTM module, meant to take advantage of temporal dependencies in our 1D time series data
 class LSTM(nn.Module):
 
     def __init__(self, input_size: int,
@@ -49,17 +59,20 @@ class LSTM(nn.Module):
         self.drop_prob = drop_prob
         self.batch_size = batch_size
 
+        # if we have specified a drop probability, then add a dropout layer
         if drop_prob:
             self.lstm = nn.LSTM(input_size, hidden_dim, num_layers, dropout=drop_prob, batch_first=True)
             self.dropout = nn.Dropout(drop_prob)
         else:
             self.lstm = nn.LSTM(input_size, hidden_dim, num_layers, batch_first=True)
 
+        # two fully connected layers at the end to obtain output
         self.fc1 = nn.Linear(hidden_dim, hidden_dim // 2)
         self.fc2 = nn.Linear(hidden_dim // 2, output_size)
 
         self.relu = nn.ReLU()
 
+    # define how data is processed through our model
     def forward(self, x):
         h_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim)
         c_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim)
